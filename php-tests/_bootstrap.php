e99d7dc340e5d0f710cc5625b0c704a258f52d38
Petr Plsek <me@kalanys.com>
2022-06-17 04:30:04 +0200

    Upload Per Partes - external autoloader need

--- php-tests/_bootstrap.php
+++ php-tests/_bootstrap.php
@@ -3,5 +3,12 @@
 define('AUTHOR_NAME', 'kalanis');
 define('PROJECT_NAME', 'UploadPerPartes');
 define('PROJECT_DIR', 'php-src');
+
+$composter = realpath(__DIR__ . '/../vendor/autoload.php');
+if ($composter) {
+    $loader = @require_once $composter;
+//    $loader->addPsr4(implode('\\', [AUTHOR_NAME, PROJECT_NAME]), __DIR__);
+}
+
 require_once __DIR__ . '/_autoload.php';
 require_once __DIR__ . '/CommonTestClass.php';
